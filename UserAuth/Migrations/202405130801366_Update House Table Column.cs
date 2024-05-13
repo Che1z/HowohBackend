8dd74bfd929686855e1d67855ee60caca3953e06
Ya-Hsien Wang <109192255+KerkerWang@users.noreply.github.com>
2024-05-13 16:03:17 +0800

    Update Migrations File

---
+++ UserAuth/Migrations/202405130801366_Update House Table Column.cs
@@ -0,0 +1,18 @@
+﻿namespace UserAuth.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateHouseTableColumn : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Houses", "district", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Houses", "district");
+        }
+    }
+}
