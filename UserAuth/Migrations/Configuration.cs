476096568e0e057055ee0e25f275ebacd39a84b1
Che1z <125649445+Che1z@users.noreply.github.com>
2024-05-16 22:26:05 +0800

    feature/getHomePageHouse

---
+++ UserAuth/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace UserAuth.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UserAuth.Models.DBModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(UserAuth.Models.DBModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
