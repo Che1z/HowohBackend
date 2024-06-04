6abfd03d428bf8b2e42d5af3eb21f50e99380583
Che1z <125649445+Che1z@users.noreply.github.com>
2024-06-04 16:28:03 +0800

    Feature contract function

--- UserAuth/Controllers/OrderController.cs
+++ UserAuth/Controllers/OrderController.cs
@@ -12,6 +12,8 @@ using UserAuth.Models;
 using UserAuth.Security;
 using System.IO;
 using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.Web;
 
 namespace UserAuth.Controllers
 {
@@ -134,10 +136,10 @@ namespace UserAuth.Controllers
                 }
                 else
                 {
-                    //string filePath = @"C:\Users\KHUser\Desktop\20240603 好窩房屋租賃合約.pdf";
-                    string filePath = @"C:\Users\Howoh\Desktop\20240603_.pdf";
-                    //string tempPDF = @"C:\Users\KHUser\Desktop\輸出契約.pdf";
-                    string tempPDF = @"C:\Users\Howoh\Desktop\Contract\NewContract.pdf";
+                    string filePath = @"C:\Users\KHUser\Desktop\20240604 好窩房屋租賃合約.pdf";
+                    //string filePath = @"C:\Users\Howoh\Desktop\20240603_.pdf";
+                    string tempPDF = @"C:\Users\KHUser\Desktop\Contract\輸出契約.pdf";
+                    //string tempPDF = @"C:\Users\Howoh\Desktop\Contract\NewContract.pdf";
 
                     using (PdfReader reader = new PdfReader(filePath))
                     {
@@ -145,7 +147,15 @@ namespace UserAuth.Controllers
                         {
                             using (PdfStamper stamper = new PdfStamper(reader, fileStream))
                             {
+                                string path = Path.Combine((HttpContext.Current.Server.MapPath("~/fonts")),"msjh.ttc");
+                                BaseFont chBaseFont = BaseFont.CreateFont($"{path},0", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+                                reader.AcroFields.AddSubstitutionFont(chBaseFont);
+
+
                                 AcroFields form = stamper.AcroFields;
+                                foreach (var ele in form.Fields.Keys) { 
+                                   form.SetFieldProperty(ele, "textfont", chBaseFont, null);
+                                }
 
                                 using (var db = new DBModel())
                                 {
@@ -164,18 +174,255 @@ namespace UserAuth.Controllers
                                                 };
                                     var orderContent = query.FirstOrDefault();
 
-                                    if (query == null)
+                                    if (orderContent == null)
                                     {
                                         return Content(HttpStatusCode.NotFound, "房屋未找到");
                                     }
 
+                                    if (orderContent.house.userIdFK.Id != userId)
+                                    {
+                                        return Content(HttpStatusCode.NotFound, "房東ID錯誤");
+                                    }
+
                                     // 驗證數據是否正確
-                                    string userName = orderContent.house.userIdFK.firstName + orderContent.house.userIdFK.lastName;
+                                    string userName = orderContent.order.contractLandlordName;
+                                    string tenentName = orderContent.order.contracttenantName;
+                                    string houseLocation = orderContent.order.contractAddress;
+                                    string housePing = orderContent.house.ping;
                                     string userCity = orderContent.house.city.ToString();
 
+                                    int leaseDuraionYearCal = (orderContent.order.leaseEndTime - orderContent.order.leaseStartTime).Days / 365;
+                                    string leaseDurationYear = leaseDuraionYearCal.ToString();
+
+                                    int leaseDuraionMonthCal = (orderContent.order.leaseEndTime - orderContent.order.leaseStartTime).Days / 30;
+                                    int leaseDurationMonthMinusYear = leaseDuraionMonthCal - leaseDuraionYearCal * 12;
+                                    string leaseDurationMonth = leaseDurationMonthMinusYear.ToString();
+
+                                    string contractStart = orderContent.order.leaseStartTime.ToString("yyyy年M月d日");
+                                    string contractEnd = orderContent.order.leaseEndTime.ToString("yyyy年M月d日");
+
+                                    int rentInt = Convert.ToInt32(orderContent.house.rent);
+                                    string rent = rentInt.ToString();
+
+                                    string paymentBeforeDate = orderContent.order.contractRentPaymentBeforeDate;
+
+                                    string securityDeposit = orderContent.house.securityDeposit.ToString();
+                                    int securityDepositAmount = 0;
+                                    if (securityDeposit == "一個月")
+                                    {
+                                        securityDepositAmount = 1;
+                                    }
+                                    else if (securityDeposit == "二個月")
+                                    {
+                                        securityDepositAmount = 2;
+                                    }
+                                    securityDepositAmount = securityDepositAmount * Convert.ToInt32(rent);
+
+                                    //付款方式
+                                    List<string> paymentMethod = new List<string> { };
+                                    if (orderContent.house.paymentMethodOfWaterBill != (paymentTypeOfWaterBill)2)
+                                    {
+                                        paymentMethod.Add("水費");
+                                    }
+                                    if (orderContent.house.paymentMethodOfManagementFee == (paymentMethodOfManagementFee)3 || orderContent.house.paymentMethodOfManagementFee == (paymentMethodOfManagementFee)2)
+                                    {
+                                        paymentMethod.Add("管理費");
+                                    }
+                                    paymentMethod.Add("電費");
+
+                                    string paymentMethods = string.Join(", ", paymentMethod);                                 
+
+
                                     // 寫入表單欄位
+
+                                    //fill_1 : 出租者
                                     form.SetField("fill_1", userName);
-                                    form.SetField("fill_2", userCity);
+                                    //fill_2 : 承租者
+                                    form.SetField("fill_2", tenentName);
+
+                                    //fill_3 : 房屋座落
+                                    form.SetField("fill_3", houseLocation);
+
+                                    //fill_4 : 承租面積
+                                    form.SetField("fill_4", $"{housePing}坪");
+
+                                    //fill_5 : 合約 (計 X 年)
+                                    form.SetField("fill_5", leaseDurationYear);
+
+                                    //fill_6 : 合約 (計 X 月)
+                                    form.SetField("fill_6", leaseDurationMonth);
+
+                                    //fill_7 : 合約 (起 - 西元)
+                                    form.SetField("fill_7", contractStart);
+
+                                    //fill_8 : 合約 (訖 - 西元)
+                                    form.SetField("fill_8", contractEnd);
+
+                                    //fill_9 : 租金 (新台幣)
+                                    form.SetField("fill_9", rentInt.ToString("N0"));
+
+                                    //fill_10 : 每月 (幾日給付甲方)
+                                    form.SetField("fill_10", paymentBeforeDate);
+
+                                    //fill_11 : 押金 (新台幣)
+                                    form.SetField("fill_11", securityDepositAmount.ToString("N0"));
+
+                                    //fill_12 : 什麼費用要由 (乙方)負擔
+                                    form.SetField("fill_12", paymentMethods.ToString());
+
+                                    //fill_13 : 水費 
+                                    //fill_14 : 水費 (繳費方式)
+                                    //fill_15 : 電費 
+                                    //fill_16 : 電費 (繳費方式)
+                                    //fill_17 : 管理費
+                                    //fill_18 : 管理費 (繳費方式)
+                                    
+                                    //只需繳交電費 
+                                    if (paymentMethod.Count == 1) {
+                                        if (orderContent.house.electricBill == (paymentTypeOfElectricBill)1)
+                                        {
+                                            //自行繳納
+                                            form.SetField("fill_13", "電費依台電計價");
+                                            form.SetField("fill_14", orderContent.house.paymentMethodOfElectricBill.ToString());
+                                        }
+                                        else {
+                                            //隨房租繳納
+                                            form.SetField("fill_13", $"電費每度{orderContent.house.electricBillPerDegree}元計價");
+                                            form.SetField("fill_14", orderContent.house.paymentMethodOfElectricBill.ToString());
+                                        }                                     
+                                    }
+
+                                    //若只繳兩種 (電費 + 水費 || 管理費)
+                                    if (paymentMethod.Count == 2) {
+                                        if (paymentMethod.Contains("水費")){
+                                            if (orderContent.house.paymentMethodOfWaterBill == (paymentTypeOfWaterBill)1)
+                                            {
+                                                form.SetField("fill_13", "水費依台水計價");
+                                                form.SetField("fill_14", "自行繳納");
+                                            }
+                                            else if(orderContent.house.paymentMethodOfWaterBill == (paymentTypeOfWaterBill)3) {
+                                                form.SetField("fill_13", $"水費自訂，每人每月{orderContent.house.waterBillPerMonth}元");
+                                                form.SetField("fill_14", "隨房租繳納");
+                                            }
+                                            if (orderContent.house.electricBill == (paymentTypeOfElectricBill)1)
+                                            {
+                                                //依台電計價
+                                                form.SetField("fill_15", "電費依台電計價");
+                                                form.SetField("fill_16", orderContent.house.paymentMethodOfElectricBill.ToString());
+                                            }
+                                            else
+                                            {
+                                                //自訂
+                                                form.SetField("fill_15", $"電費每度{orderContent.house.electricBillPerDegree}元計價");
+                                                form.SetField("fill_16", orderContent.house.paymentMethodOfElectricBill.ToString());
+                                            }
+                                        }
+                                        if (paymentMethod.Contains("管理費")) {
+
+                                            int managementFee = Convert.ToInt32(orderContent.house.managementFeePerMonth);
+                                            form.SetField("fill_13", $"管理費每月{managementFee.ToString("N0")}元");
+                                            form.SetField("fill_14", orderContent.house.paymentMethodOfManagementFee.ToString());
+                                            if (orderContent.house.electricBill == (paymentTypeOfElectricBill)1)
+                                            {
+                                                //依台電計價
+                                                form.SetField("fill_15", "電費依台電計價");
+                                                form.SetField("fill_16", orderContent.house.paymentMethodOfElectricBill.ToString());
+                                            }
+                                            else
+                                            {
+                                                //自訂
+                                                form.SetField("fill_15", $"電費每度{orderContent.house.electricBillPerDegree}元計價");
+                                                form.SetField("fill_16", orderContent.house.paymentMethodOfElectricBill.ToString());
+                                            }
+                                        }                                    
+                                    }
+                                    if (paymentMethod.Count == 3) {
+                                        if (orderContent.house.paymentMethodOfWaterBill == (paymentTypeOfWaterBill)1)
+                                        {
+                                            form.SetField("fill_13", "水費依台水計價");
+                                            form.SetField("fill_14", "自行繳納");
+                                        }
+                                        else if(orderContent.house.paymentMethodOfWaterBill == (paymentTypeOfWaterBill)3)
+                                        {
+                                            form.SetField("fill_13", $"水費自訂，每人每月{orderContent.house.waterBillPerMonth}元");
+                                            form.SetField("fill_14", "隨房租繳納");
+                                        }
+                                        int managementFee = Convert.ToInt32(orderContent.house.managementFeePerMonth);
+                                        form.SetField("fill_15", $"管理費每月{managementFee.ToString("N0")}元");
+                                        form.SetField("fill_16", orderContent.house.paymentMethodOfManagementFee.ToString());
+
+                                        if (orderContent.house.electricBill == (paymentTypeOfElectricBill)1)
+                                        {
+                                            //自行繳納
+                                            form.SetField("fill_17", "電費依台電計價");
+                                            form.SetField("fill_18", orderContent.house.paymentMethodOfElectricBill.ToString());
+                                        }
+                                        else
+                                        {
+                                            //隨房租繳納
+                                            form.SetField("fill_17", $"電費每度{orderContent.house.electricBillPerDegree}元計價");
+                                            form.SetField("fill_18", orderContent.house.paymentMethodOfElectricBill.ToString());
+                                        }
+                                    }
+
+                                    //fill_1_2 : 提前幾個月通知他方
+                                    form.SetField("fill_1_2", orderContent.order.contractTerminationNoticeMonths);
+
+                                    //fill_2_2 : 賠償幾個月的租金
+                                    form.SetField("fill_2_2", orderContent.order.contractTerminationPenaltyMonths);
+
+                                    //fill_3_2 : 特別約定事項2 (不用填)
+                                    //fill_4_2 : 特別約定事項3 (不用填)
+                                    //fill_5_2 : 生活公約1 (不用填)
+                                    //fill_6_2 : 生活公約2 (不用填)
+                                    //fill_7_2 : 設備包含
+                                    List<string> equipments = new List<string> { };
+                                    if (orderContent.house.hasAirConditioner == true) {
+                                        equipments.Add("冷氣");
+                                    }
+                                    if (orderContent.house.hasWashingMachine == true)
+                                    {
+                                        equipments.Add("洗衣機");
+                                    }
+                                    if (orderContent.house.hasRefrigerator == true)
+                                    {
+                                        equipments.Add("冰箱");
+                                    }
+                                    if (orderContent.house.hasCloset == true)
+                                    {
+                                        equipments.Add("衣櫃");
+                                    }
+                                    if (orderContent.house.hasTableAndChair == true)
+                                    {
+                                        equipments.Add("桌椅");
+                                    }
+                                    if (orderContent.house.hasWaterHeater == true)
+                                    {
+                                        equipments.Add("熱水器");
+                                    }
+                                    if (orderContent.house.hasInternet == true)
+                                    {
+                                        equipments.Add("網路");
+                                    }
+                                    if (orderContent.house.hasBed == true)
+                                    {
+                                        equipments.Add("床");
+                                    }
+                                    if (orderContent.house.hasTV == true)
+                                    {
+                                        equipments.Add("電視");
+                                    }
+                                    string equipmentString = string.Join(", ", equipments);
+                                    form.SetField("fill_7_2", equipmentString);
+
+                                    //fill_8_2 : 備註 (不用填)
+                                    //fill_9_2 : 甲方 (不用填)
+                                    //fill_10_2 : 乙方 (不用填)
+                                    //fill_11_2 : 身分證字號(甲方) (不用填)
+                                    //fill_12_2 : 身分證字號(乙方) (不用填)
+                                    //fill_13_2 : 合約 (西元) (不用填)
+                                    //fill_14_2 : 合約 (月) (不用填)
+                                    //fill_15_2 : 合約 (日) (不用填)
                                 }
                                 stamper.FormFlattening = true;
                             }
@@ -206,7 +453,8 @@ namespace UserAuth.Controllers
             }
         }
 
-       
+     
+
 
 
 
