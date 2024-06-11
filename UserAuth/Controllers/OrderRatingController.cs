f8d95cd913b4be1602dbff2d32700f4f247b70ff
Ya-Hsien Wang <109192255+KerkerWang@users.noreply.github.com>
2024-06-11 17:10:42 +0800

    add GetMyCommentList

--- UserAuth/Controllers/OrderRatingController.cs
+++ UserAuth/Controllers/OrderRatingController.cs
@@ -265,6 +265,238 @@ namespace UserAuth.Controllers
             }
         }
 
+        /// <summary>
+        /// [ACC-1]顯示全部評價
+        /// </summary>
+        /// <param name="page">頁數</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/comment/common/list/all")]
+        [JwtAuthFilters]
+        public IHttpActionResult GetMyCommentList(string page = "1")
+        {
+            //取得使用者JWT
+            var jwtObject = JwtAuthFilters.GetToken(Request.Headers.Authorization.Parameter);
+
+            //取得JWT內部資料
+            int UserId = (int)jwtObject["Id"];
+            UserRoleType UserRole = (UserRoleType)jwtObject["Role"];
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    // 假設 page 是傳入的頁碼參數，pageSize 是每頁顯示的筆數
+                    int pageNumber;
+                    // 嘗試將 page 轉換成 int，如果失敗則設置為預設值 1
+                    if (!int.TryParse(page, out pageNumber) || pageNumber < 1)
+                    {
+                        pageNumber = 1;
+                    }
+                    int pageSize = 12; // 每頁顯示的筆數
+                    if (UserRole == UserRoleType.租客)
+                    {
+                        var query = from order in db.OrdersEntities.AsQueryable()
+                                    where order.userId == UserId && order.status == OrderStatus.租客已確認租約
+                                    join house in db.HouseEntities on order.houseId equals house.id
+                                    join user in db.UserEntities on house.userId equals user.Id
+                                    join houseImg in db.HouseImgsEntities on house.id equals houseImg.houseId
+                                    where houseImg.isCover == true
+                                    select new
+                                    {
+                                        order,
+                                        house,
+                                        photo = houseImg.path,
+                                        landlord = user,
+                                        myComment = (from orderRating in db.OrdersRatingEntities
+                                                     join reply in db.ReplyRatingEntities on orderRating.id equals reply.orderRatingId into replyGroup
+                                                     from reply in replyGroup.DefaultIfEmpty()
+                                                     where orderRating.UserId == UserId && orderRating.orderId == order.id
+                                                     select new
+                                                     {
+                                                         comment = orderRating,
+                                                         reply
+                                                     }).FirstOrDefault(),
+                                        landlordComment = (from orderRating in db.OrdersRatingEntities
+                                                           join reply in db.ReplyRatingEntities on orderRating.id equals reply.orderRatingId into replyGroup
+                                                           from reply in replyGroup.DefaultIfEmpty()
+                                                           where orderRating.UserId != UserId && orderRating.orderId == order.id
+                                                           select new
+                                                           {
+                                                               comment = orderRating,
+                                                               reply
+                                                           }).FirstOrDefault()
+                                    };
+                        // 計算資料總筆數
+                        int totalRecords = query.Count();
+                        // 分頁
+                        var queryResult = query.OrderByDescending(o => o.order.leaseEndTime).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+                        //var queryResult = query.ToList();
+                        var orderList = new List<object>();
+                        var expiredDate = DateTime.Today.AddDays(-14);
+                        foreach (var i in queryResult)
+                        {
+                            bool canComment = true;
+                            if (expiredDate > i.order.leaseEndTime || i.myComment != null)
+                            {
+                                canComment = false;
+                            }
+                            var order = new
+                            {
+                                orderInfo = new
+                                {
+                                    orderId = i.order.id,
+                                    photo = i.photo,
+                                    name = i.house.name,
+                                    landlord = i.landlord.lastName + i.landlord.firstName,
+                                    leaseStartTime = i.order.leaseStartTime,
+                                    leaseEndTime = i.order.leaseEndTime
+                                },
+                                commentInfo = new
+                                {
+                                    canComment = canComment,
+                                    myComment = i.myComment == null ? null : new
+                                    {
+                                        commentId = i.myComment.comment.id,
+                                        rating = i.myComment.comment.Rating,
+                                        comment = i.myComment.comment.Comment,
+                                        commentTime = i.myComment.comment.RatingDate,
+                                        reply = i.myComment.reply?.ReplyComment,
+                                        replyTime = i.myComment.reply?.ReplyDate
+                                    },
+                                    landlordComment = i.landlordComment == null ? null : new
+                                    {
+                                        commentId = i.landlordComment.comment.id,
+                                        rating = i.landlordComment.comment.Rating,
+                                        comment = i.landlordComment.comment.Comment,
+                                        commentTime = i.landlordComment.comment.RatingDate,
+                                        reply = i.landlordComment.reply?.ReplyComment,
+                                        replyTime = i.landlordComment.reply?.ReplyDate
+                                    },
+                                }
+                            };
+                            orderList.Add(order);
+                        }
+                        var result = new
+                        {
+                            statusCode = 200,
+                            status = "success",
+                            message = "已成功回傳租客的評價列表",
+                            data = new
+                            {
+                                page = pageNumber,
+                                totalCount = totalRecords,
+                                orderList = orderList
+                            }
+                        };
+                        return Content(HttpStatusCode.OK, result);
+                    }
+                    else
+                    {
+                        var query = from house in db.HouseEntities.AsQueryable()
+                                    where house.userId == UserId
+                                    join order in db.OrdersEntities on house.id equals order.houseId
+                                    where order.status == OrderStatus.租客已確認租約 || order.status == OrderStatus.租客非系統用戶
+                                    join user in db.UserEntities on house.userId equals user.Id into userGroup
+                                    from user in userGroup.DefaultIfEmpty()
+                                    join houseImg in db.HouseImgsEntities on house.id equals houseImg.houseId
+                                    where houseImg.isCover == true
+                                    select new
+                                    {
+                                        order,
+                                        house,
+                                        photo = houseImg.path,
+                                        tenant = order.status == OrderStatus.租客非系統用戶 ? null : user,
+                                        myComment = (from orderRating in db.OrdersRatingEntities
+                                                     join reply in db.ReplyRatingEntities on orderRating.id equals reply.orderRatingId into replyGroup
+                                                     from reply in replyGroup.DefaultIfEmpty()
+                                                     where orderRating.UserId == UserId && orderRating.orderId == order.id
+                                                     select new
+                                                     {
+                                                         comment = orderRating,
+                                                         reply
+                                                     }).FirstOrDefault(),
+                                        tenantComment = (from orderRating in db.OrdersRatingEntities
+                                                         join reply in db.ReplyRatingEntities on orderRating.id equals reply.orderRatingId into replyGroup
+                                                         from reply in replyGroup.DefaultIfEmpty()
+                                                         where orderRating.UserId != UserId && orderRating.orderId == order.id
+                                                         select new
+                                                         {
+                                                             comment = orderRating,
+                                                             reply
+                                                         }).FirstOrDefault()
+                                    };
+                        // 計算資料總筆數
+                        int totalRecords = query.Count();
+                        // 分頁
+                        var queryResult = query.OrderByDescending(o => o.order.leaseEndTime).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+                        //var queryResult = query.ToList();
+                        var orderList = new List<object>();
+                        var expiredDate = DateTime.Today.AddDays(-14);
+                        foreach (var i in queryResult)
+                        {
+                            bool canComment = true;
+                            if (expiredDate > i.order.leaseEndTime || i.myComment != null || i.tenant == null)
+                            {
+                                canComment = false;
+                            }
+                            var order = new
+                            {
+                                orderInfo = new
+                                {
+                                    orderId = i.order.id,
+                                    photo = i.photo,
+                                    name = i.house.name,
+                                    tenant = i.tenant == null ? null : i.tenant.lastName + i.tenant.firstName,
+                                    leaseStartTime = i.order.leaseStartTime,
+                                    leaseEndTime = i.order.leaseEndTime
+                                },
+                                commentInfo = new
+                                {
+                                    canComment = canComment,
+                                    myComment = i.myComment == null ? null : new
+                                    {
+                                        commentId = i.myComment.comment.id,
+                                        rating = i.myComment.comment.Rating,
+                                        comment = i.myComment.comment.Comment,
+                                        commentTime = i.myComment.comment.RatingDate,
+                                        reply = i.myComment.reply?.ReplyComment,
+                                        replyTime = i.myComment.reply?.ReplyDate
+                                    },
+                                    tenantComment = i.tenantComment == null ? null : new
+                                    {
+                                        commentId = i.tenantComment.comment.id,
+                                        rating = i.tenantComment.comment.Rating,
+                                        comment = i.tenantComment.comment.Comment,
+                                        commentTime = i.tenantComment.comment.RatingDate,
+                                        reply = i.tenantComment.reply?.ReplyComment,
+                                        replyTime = i.tenantComment.reply?.ReplyDate
+                                    },
+                                }
+                            };
+                            orderList.Add(order);
+                        }
+                        var result = new
+                        {
+                            statusCode = 200,
+                            status = "success",
+                            message = "已成功回傳租客的評價列表",
+                            data = new
+                            {
+                                page = pageNumber,
+                                totalCount = totalRecords,
+                                orderList = orderList
+                            }
+                        };
+                        return Content(HttpStatusCode.OK, result);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
         // GET: api/OrderRating
         //public IEnumerable<string> Get()
         //{
